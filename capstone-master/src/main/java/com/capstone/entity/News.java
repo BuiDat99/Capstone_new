87fc405d1504dd69db5c6a44e92eac298e3d21e8
BuiDat99 <63451376+BuiDat99@users.noreply.github.com>
2020-09-08 16:54:02 +0700

    database

--- capstone-master/src/main/java/com/capstone/entity/News.java
+++ capstone-master/src/main/java/com/capstone/entity/News.java
@@ -12,56 +12,55 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Posts")
-public class News implements Serializable{
+@Table(name = "Posts")
+public class News implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "post_Id", nullable = false)
 	private int id;
-	
+
 	@Column(name = "title", length = 255, nullable = false)
 	private String title;
-	
+
 	@Column(name = "image_Title", length = 50)
 	private String imageTitle;
-	
+
 	@Column(name = "poster", length = 255, nullable = false)
 	private String poster;
-	
+
 	@Column(name = "author", length = 255, nullable = false)
 	private String author;
-	
-	
+
 	@Column(name = "short_Description", length = 1000, nullable = false)
 	private String shortDescription;
-	
-	@Column(name = "detail_Descripton", length = 13000, nullable = false)
+
+	@Column(name = "detail_Descripton", length = 30000, nullable = false)
 	private String detailDescription;
-	
+
 	@Column(name = "source", length = 255, nullable = false)
 	private String source;
-	
+
 	@Column(name = "hashtag", length = 50, nullable = false)
 	private String hashtag;
-	
+
 	@Column(name = "status", length = 50, nullable = false)
 	private String status;
-	
+
 	@Column(name = "modified_Date", nullable = false)
 	private String modifiedDate;
-	
+
 	@Column(name = "creation_Date", nullable = false)
 	private String creationDate;
-	
+
 	@ManyToOne
-	@JoinColumn(name="category_Id")
+	@JoinColumn(name = "category_Id")
 	private NewCategory category;
-	
+
 	@ManyToOne
-	@JoinColumn(name="user_Id")
+	@JoinColumn(name = "user_Id")
 	private AppUser appUser;
 
 	public int getId() {
@@ -175,7 +174,5 @@ public class News implements Serializable{
 	public void setAppUser(AppUser appUser) {
 		this.appUser = appUser;
 	}
-	
-	
-	
+
 }
